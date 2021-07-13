5cdbf32cb75303b288990d062cc9b36c19612e44
Eric White <47019383+edwhite12@users.noreply.github.com>
2021-07-13 16:38:55 -0500

    debug

--- WMsav_sav.f90
+++ WMsav_sav.f90
@@ -53,7 +53,8 @@ subroutine sav
     real(sp) :: prior,pri,prs                                           ! prior probability for SAV based on location and marsh vegetation of nearest land
     real(sp) :: prob                                                    ! probability of SAV in grid cell
     real(sp),dimension(:),allocatable :: grid_dtl                       ! local array to store minimum distance to land for each LAVegMod grid cell
-
+    integer :: def_val
+    
     allocate(grid_dtl(ngrid))
     grid_dtl = maxval(dem_dtl)                                          ! initialize the whole grid_dtl array to the maximum distance-to-land values found in the pixel-level distance-to-land array
     
@@ -65,7 +66,7 @@ subroutine sav
     write(*,*)spsal_params(1),spsal_params(2),spsal_params(3)
     
     open(unit=8888, file = trim(adjustL(grid_sav_file) ))
-    write(8888,'(A)') 'gridID,comp,er_n,SAV_presence,SAV_prob,mean_spring_sal_ppt,mean_spring_tss_mgL,distance_to_land_m,FFIBS,ans1,ans0,prior,prior_int,prior_slp,'
+    write(8888,'(A)') 'gridID,comp,er_n,SAV_presence,SAV_prob,mean_spring_sal_ppt,mean_spring_tss_mgL,distance_to_land_m,FFIBS,ans1,ans0,prior,prior_int,prior_slp,ans1_dfl_part,ans0_dfl_part,ans1_sal_part,ans0_sal_part,ans1_tss_part,ans0_tss_part'
     
     ! assign minimum distance-to-land found in each ICM-LAVegMod grid cell
     do i=1,ndem
@@ -86,82 +87,72 @@ subroutine sav
         dfl = -9999
         ffibs = -9999
         prob = -9999 
-         
+        pres = -9999
+        
         c = grid_comp(ig)
         if (c > 0) then
             en = comp_eco(c)
             if (en > 0) then
                 dfl = grid_dtl(ig)
                 ffibs = grid_FIBS_score(ig)
-                if (dfl > 2010) then            ! grid cell is further than 2 km from land - too much exposure for SAV cannot occur
-                    prob = 0.0
-                    pres = 0
-                    ans1 = 0.0
-                    ans0 = 0.0
-                    prior = 0.0
-                else if (dfl <= 0) then         ! grid cell is 100% land/marsh so SAV cannot occur
-                    prob = 0.0
-                    pres = 0
-                    ans1 = 0.0
-                    ans0 = 0.0
-                    prior = 0.0
-                else if (dfl <= 2010) then      ! grid cell has some water that is less than 2 km from nearest land - calculate SAV probability
-                    spsal = 5.0!( sal_av_mons(c,3)+sal_av_mons(c,4)+sal_av_mons(c,5) ) / 3.0
-                    sptss = 50.0!( tss_av_mons(c,3)+tss_av_mons(c,4)+tss_av_mons(c,5) ) / 3.0
-                
-                    ! spring salinity SAV probability 
-                    ans1_sal_part = 1/(spsal_params(3)*SQRT(2.0*pi))*EXP(-0.5*((LOG(spsal)-(spsal_params(1)+spsal_params(2)))/spsal_params(3))**2)
-                    ans0_sal_part = 1/(spsal_params(3)*SQRT(2.0*pi))*EXP(-0.5*((LOG(spsal)-(spsal_params(1)))/spsal_params(3))**2)
-                    
-                    ! distance from land SAV probability
-                    ans1_dfl_part = 1/(dfl_params(3)*SQRT(2.0*pi))*EXP(-0.5*((LOG(dfl)-(dfl_params(1)+dfl_params(2)))/dfl_params(3))**2)
-                    ans0_dfl_part = 1/(dfl_params(3)*SQRT(2.0*pi))*EXP(-0.5*((LOG(dfl)-(dfl_params(1)))/dfl_params(3))**2)
-                    
-                    ! spring TSS SAV probability
-                    ans1_tss_part = 1/(sptss_params(3)*SQRT(2.0*pi))*EXP(-0.5*((LOG(sptss)-(sptss_params(1)+sptss_params(2)))/sptss_params(3))**2)
-                    ans0_tss_part = 1/(sptss_params(3)*SQRT(2.0*pi))*EXP(-0.5*((LOG(sptss)-(sptss_params(1)))/sptss_params(3))**2)
-
-                    ! calculate prior probability from location/veg type
-                    pri = prior_int(en)
-                    prs = prior_slope(en)
-                    prior = 1/(1+EXP(-(prior_int(en)+prior_slope(en)*ffibs)))
-                    
-                    ! combine info from predictors
-                    ans1 = ans1_sal_part*ans1_dfl_part*ans1_tss_part
-                    ans0 = ans0_sal_part*ans0_dfl_part*ans0_tss_part
-                    
-                    prob = prior*ans1/(prior*ans1+(1-prior)*ans0)
-                    
-                    ! convert probability of SAV to presence/absence
-                    if ( LOG(prior*ans1) > LOG(1-prior)*ans0 ) then
-                        pres = 1
-                    else
+                if (ffibs <> -9999) then
+                    if (dfl > 2010) then            ! grid cell is further than 2 km from land - too much exposure for SAV cannot occur
+                        prob = 0.0
                         pres = 0
+                        ans1 = 0.0
+                        ans0 = 0.0
+                        prior = 0.0
+                    else if (dfl <= 0) then         ! grid cell is 100% land/marsh so SAV cannot occur
+                        prob = 0.0
+                        pres = 0
+                        ans1 = 0.0
+                        ans0 = 0.0
+                        prior = 0.0
+                    else if (dfl <= 2010) then      ! grid cell has some water that is less than 2 km from nearest land - calculate SAV probability
+                        spsal = 5.0!( sal_av_mons(c,3)+sal_av_mons(c,4)+sal_av_mons(c,5) ) / 3.0
+                        sptss = 50.0!( tss_av_mons(c,3)+tss_av_mons(c,4)+tss_av_mons(c,5) ) / 3.0
+                    
+                        ! spring salinity SAV probability 
+                        ans1_sal_part = 1/(spsal_params(3)*SQRT(2.0*pi))*EXP(-0.5*((LOG(spsal)-(spsal_params(1)+spsal_params(2)))/spsal_params(3))**2)
+                        ans0_sal_part = 1/(spsal_params(3)*SQRT(2.0*pi))*EXP(-0.5*((LOG(spsal)-(spsal_params(1)))/spsal_params(3))**2)
+                        
+                        ! distance from land SAV probability
+                        ans1_dfl_part = 1/(dfl_params(3)*SQRT(2.0*pi))*EXP(-0.5*((LOG(dfl)-(dfl_params(1)+dfl_params(2)))/dfl_params(3))**2)
+                        ans0_dfl_part = 1/(dfl_params(3)*SQRT(2.0*pi))*EXP(-0.5*((LOG(dfl)-(dfl_params(1)))/dfl_params(3))**2)
+                        
+                        ! spring TSS SAV probability
+                        ans1_tss_part = 1/(sptss_params(3)*SQRT(2.0*pi))*EXP(-0.5*((LOG(sptss)-(sptss_params(1)+sptss_params(2)))/sptss_params(3))**2)
+                        ans0_tss_part = 1/(sptss_params(3)*SQRT(2.0*pi))*EXP(-0.5*((LOG(sptss)-(sptss_params(1)))/sptss_params(3))**2)
+    
+                        ! calculate prior probability from location/veg type
+                        pri = prior_int(en)
+                        prs = prior_slope(en)
+                        prior = 1/(1+EXP(-(prior_int(en)+prior_slope(en)*ffibs)))
+                        
+                        ! combine info from predictors
+                        ans1 = ans1_sal_part*ans1_dfl_part*ans1_tss_part
+                        ans0 = ans0_sal_part*ans0_dfl_part*ans0_tss_part
+                        
+                        prob = prior*ans1/(prior*ans1+(1-prior)*ans0)
+                        
+                        ! convert probability of SAV to presence/absence
+                        if ( LOG(prior*ans1) > LOG(1-prior)*ans0 ) then
+                            pres = 1
+                        else
+                            pres = 0
+                        end if
+                        write(8888,9999) ig,c,en,pres,prob,spsal,sptss,dfl,ffibs,ans1,ans0,prior,pri,prs,ans1_dfl_part,ans0_dfl_part,ans1_sal_part,ans0_sal_part,ans1_tss_part,ans0_tss_part
                     end if
                 end if
-            else
-                prob = -9999.0
-                pres = -9999
-                pri = -9999
-                prs = -9999
-                prior = -9999
             end if
-        else
-            prob = -9999.0
-            pres = -9999
-            pri = -9999
-            prs = -9999
-            prior = -9999
         end if
-
-
-        write(8888,9999) ig,c,en,pres,prob,spsal,sptss,dfl,ffibs,ans1,ans0,prior,pri,prs
-    
+        def_val = -9999
+        write(8888,9990) ig,c,en,pres,prob,spsal,sptss,dfl,ffibs,def_val,def_val,def_val,def_val,def_val,def_val,def_val,def_val,def_val,def_val,def_val  
     end do
     
     close(8888)
     
-9999 format( 5(I0,','), F0.4, 8(',',F0.4) )
-     
+9999 format( 5(I0,','), F0.4, 14(',',F0.4) )
+9990 format( 19(I0,','), I0 )  
     return
 end
