ea99168df11c76d1a00bf7313495f7e6d095fe00
Eric White <47019383+edwhite12@users.noreply.github.com>
2021-07-16 10:41:14 -0500

    testing

--- WMsav_distance_to_land.f90
+++ WMsav_distance_to_land.f90
@@ -46,80 +46,60 @@ subroutine distance_to_land
     integer :: land_ho                                                          ! flag set to 1 when land is found in current concentric ring being looped over
     real(sp) :: local_dtl                                                       ! local distance to land for pixel d00 when land is found at pixel dxx
     real(sp) :: ring_dtl_mn                                                     ! minimum distance-to-land for the current concentric ring
-    integer :: bins                                                             ! number of bins to report out progress of looping
     integer ::  pn                                                              ! iterator
-    integer ::np                                                                ! counter tracking looping progress
     integer :: nprogress                                                        ! progress of looping in pixel count
-    integer :: progress                                                         ! progress of looping as percentage
-    integer,dimension(:),allocatable :: progress_bin                            ! array to track progress of loops for report out
 
-    ws = 67                                                                     ! maximum distance (in DEM pixels) to search for land
+    ws = 5 !67                                                                     ! maximum distance (in DEM pixels) to search for land
     
     write(  *,'(A,I6,A)') ' - determining distance to land for each DEM pixel (search limited to ',ws*dem_res,' m)'
     write(000,'(A,I6,A)') ' - determining distance to land for each DEM pixel (search limited to ',ws*dem_res,' m)'
     
-    dem_dtl = 500.
-    !dem_dtl = ws*dem_res+1                                                      ! initialize distance to land for each DEM pixel to maximum distance-to-land value
-    !                                                                            ! if ws is 67 and dem_res is 30, than the maximum distance searched will be 2010
-    !                                                                            ! therefore, if the entire search window was examined and no land was found, 
-    !                                                                            ! the distance to land will be set equal to 2011 (e.g. more than 2010 meters to land)
-    !bins = 100
-    !allocate(progress_bin(bins))
-    !do pn = 1,bins
-    !    progress_bin(pn) = pn*ndem/bins
-    !end do
-    !nprogress = 0
-    !np = 0
-    !
-    !do ic = ws+1,n_dem_col-ws-1                                                 ! loop over mapped array of pixel index values
-    !    do ir = ws+1,n_dem_row-ws-1                                             ! this will not allow any pixel on the outside border of the raster be included in search
-    !        
-    !        nprogress = nprogress + 1
-    !        if (nprogress == 100000) then
-    !            write(*,*) 'finished checking 100k pixels'
-    !            nprogress=0
-    !        end if
-    !        
-!   !         progress = 100*nprogress/ndem
-!   !        
-!   !         if (ANY(progress_bin == progress) ) then
-!   !             write(  *,'(A,I3)')  ' progress: ',progress_bin(np)
-!   !             write(000,'(A,I3)')  ' progress: ',progress_bin(np)
-!   !             np = np+1
-!   !         end if
-    !        
-    !        d00 = dem_index_mapped(ic, ir)                                      ! current pixel being calculated is d00 with grid coordinates of (ic,ir)
-    !        
-    !        
-    !        
-    !        if (dem_lndtyp(d00) /= dem_NoDataVal) then                          ! check if dem_lndtyp is assigned - if not, pixel is skipped
-    !            if (dem_lndtyp(d00) /= 2) then                                  ! check if dem_lndtyp is water, if not water, set distance to land to zero - and move on to next pixel
-    !                dem_dtl(d00) = 0
-    !            else                                                            ! pixel d00 is water, so start looking for land
-    !                do ring = 1,ws                                              ! start search concentrically moving outward from d00 one ring at a time until max windowsize is reached
-    !                    ring_dtl_mn = ws*dem_res+1                              ! initialize the minimum distance to land for the current search ring to maximum search distance+1 meter
-    !                    land_ho = 0                                             ! set flag to 0 for start of ring
-    !                    do rc = -ring,ring                                      ! local counter for columns in current concentric ring
-    !                        do rr = -ring,ring                                  ! local counter for rows in current concentric ring
-    !                            dxx = dem_index_mapped(ic+rc, ir+rr)            ! pixel in current concentric ring with grid coordinates of (ic+rc,ir+rr)
-    !                            if (dem_lndtyp(dxx) /= 2) then                  ! LAND HO !!!
-    !                                land_ho = 1
-    !                                local_dtl = sqrt( float((rc*dem_res)**2) + float((rr*dem_res)**2) ) ! calculated distance to land for current pixel where land was found (this is only first land pixel found in current ring - there may be a closer one later in the same ring, so keep looping over current ring)
-    !                                ring_dtl_mn = min(ring_dtl_mn, local_dtl)                           ! update minimum distance to land for current ring
-    !                                dem_dtl(ic) = ring_dtl_mn                                           ! update the distance to land value for the current DEM pixel to the current minimum
-    !                            end if
-    !                        end do
-    !                    end do
-    !                    
-    !                    if (land_ho == 1) then                                  ! finished searching over the current concentric ring - break do loop if land was found on ring
-    !                        exit
-    !                    end if
-    !                    
-    !                end do
-    !            end if
-    !        end if
-    !    end do
-    !end do
-!
+    !dem_dtl = 500.
+    dem_dtl = ws*dem_res+1                                                      ! initialize distance to land for each DEM pixel to maximum distance-to-land value
+                                                                                ! if ws is 67 and dem_res is 30, than the maximum distance searched will be 2010
+                                                                                ! therefore, if the entire search window was examined and no land was found, 
+                                                                                ! the distance to land will be set equal to 2011 (e.g. more than 2010 meters to land)
+    nprogress = 0
+    
+    do ic = ws+1,n_dem_col-ws-1                                                 ! loop over mapped array of pixel index values
+        do ir = ws+1,n_dem_row-ws-1                                             ! this will not allow any pixel on the outside border of the raster be included in search
+            
+            nprogress = nprogress + 1
+            if (nprogress == 100000) then
+                write(*,*) 'finished checking 100k pixels'
+                nprogress=0
+            end if
+           
+            d00 = dem_index_mapped(ic, ir)                                      ! current pixel being calculated is d00 with grid coordinates of (ic,ir)
+            
+            if (dem_lndtyp(d00) /= dem_NoDataVal) then                          ! check if dem_lndtyp is assigned - if not, pixel is skipped
+                if (dem_lndtyp(d00) /= 2) then                                  ! check if dem_lndtyp is water, if not water, set distance to land to zero - and move on to next pixel
+                    dem_dtl(d00) = 0
+                else                                                            ! pixel d00 is water, so start looking for land
+                    do ring = 1,ws                                              ! start search concentrically moving outward from d00 one ring at a time until max windowsize is reached
+                        ring_dtl_mn = ws*dem_res+1                              ! initialize the minimum distance to land for the current search ring to maximum search distance+1 meter
+                        land_ho = 0                                             ! set flag to 0 for start of ring
+                        do rc = -ring,ring                                      ! local counter for columns in current concentric ring
+                            do rr = -ring,ring                                  ! local counter for rows in current concentric ring
+                                dxx = dem_index_mapped(ic+rc, ir+rr)            ! pixel in current concentric ring with grid coordinates of (ic+rc,ir+rr)
+                                if (dem_lndtyp(dxx) /= 2) then                  ! LAND HO !!!
+                                    land_ho = 1
+                                    local_dtl = sqrt( float((rc*dem_res)**2) + float((rr*dem_res)**2) ) ! calculated distance to land for current pixel where land was found (this is only first land pixel found in current ring - there may be a closer one later in the same ring, so keep looping over current ring)
+                                    ring_dtl_mn = min(ring_dtl_mn, local_dtl)                           ! update minimum distance to land for current ring
+                                    dem_dtl(ic) = ring_dtl_mn                                           ! update the distance to land value for the current DEM pixel to the current minimum
+                                end if
+                            end do
+                        end do
+                        
+                        if (land_ho == 1) then                                  ! finished searching over the current concentric ring - break do loop if land was found on ring
+                            exit
+                        end if
+                        
+                    end do
+                end if
+            end if
+        end do
+    end do
+
     return
 end
